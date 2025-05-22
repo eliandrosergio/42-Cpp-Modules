92e8f693608629ae5e6f95db86668b5928f4d533
Eliandro Faustino <efaustin@c1r11s7.42luanda.com>
2025-05-22 12:05:42 +0100

    limpando o cpp01

--- Cpp Module 01/ex00/newZombie.cpp
+++
@@ -1,9 +0,0 @@
-#include "Zombie.h"
-
-// Alocaçcão para uso fora do seu escopo (head)
-
-// Criador de novo Zombie
-Zombie* newZombie( std::string name ) {
-	Zombie* tempZombie = new Zombie(name);
-	return (tempZombie);
-}
