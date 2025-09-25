f8cdffb61f49a00b349fdb414c6586fab0013b62
Eliandro Faustino <efaustin@c2r1s4.42luanda.com>
2025-09-25 16:11:52 +0100

    reposiçcão

--- Cpp Module 01/ex00/randomChump.cpp
+++
@@ -1,9 +0,0 @@
-#include "Zombie.h"
-
-// Alocaçcão para uso no seu escopo (stack)
-
-// Criandor e anuciador do Zombie aleatório
-void randomChump( std::string name ) {
-	Zombie tempZombie(name);
-	tempZombie.announce();
-}
